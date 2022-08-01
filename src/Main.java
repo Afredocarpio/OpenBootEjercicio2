b64d2a4790501e4e0fcd78ecc720490af5a250af
alfredo <alfredocarpioo@gmail.com>
2022-08-01 11:47:04 -0500

    Ejercicio Estructuras de Control

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,9 @@ public class Main {
         int numeroif = num.nextInt();*/
 
         /*Paso 1*/
+        /*Usando un if, crear una condición que compare si la variable numeroIf es positivo, negativo, o 0.
+        Pista: Los números inferiores a 0 son negativos y los superiores, positivos.*/
+
         int numeroif = 21;
 
        if (numeroif > 0){
@@ -18,6 +21,11 @@ public class Main {
        }
 
         /*Paso 2*/
+        /*Crea un bucle While, este bucle tendrá que tener como condición que la variable numeroWhile sea inferior a 3,
+        el bloque de código que tendrá el bucle deberá:
+        * Incrementar el valor de la variable en uno cada vez que se ejecute.
+        * Mostrarlo por pantalla cada vez que se ejecute.*/
+
         int numeroWhile = 0;
         while (numeroWhile < 3) {
             System.out.println("Respuesta While : " + numeroWhile);
@@ -25,6 +33,8 @@ public class Main {
         }
 
         /*Paso 3*/
+        /*Para el bucle Do While, deberás crear la misma estructura que en el While,
+        pero solo se debe ejecutar una vez.*/
 
         int numDoWhile = 2;
         do {
@@ -34,11 +44,20 @@ public class Main {
         while (numDoWhile < 3);
 
         /*Paso 3*/
+        /*Para el bucle For, crea una variable numeroFor, esta variable tendrá como valor 0
+        y su condición será que la variable sea igual o menor que 3,
+         se irá incrementando en 1 su valor cada vez que se ejecute y deberá mostrarse por pantalla.*/
+
         for (int numeroFor = 0; numeroFor <= 3; numeroFor++) {
             System.out.println("Respuesta For: " + numeroFor);
         }
 
         /*Paso Cuatro*/
+        /*Por último, para el Switch, deberás crear la variable estacion,
+         y distintos case para las cuatro estaciones del año.
+         Dependiendo del valor de la variable estacion se deberá mandar un mensaje por consola informando
+         de la estación en la que está. También habrá que poner un default para cuando el valor de la variable
+         no sea una estación.*/
 
         String estacion = "primavera";
         switch (estacion) {
